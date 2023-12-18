217395bf2f10cd8548bc1f197dcb815f923da1dd
Berna UZUNOĞLU <bernauzunoglu58@gmail.com>
2023-12-18 14:47:03 +0300

    First add commit

---
+++ Python_Objeleri/Type_Conversion.py
@@ -0,0 +1,36 @@
+'''
+x = input('Sayi 1 : ')
+y = input('Sayi 2 : ')
+
+print(type(x))
+print(type(y))
+
+toplam = int(x) + int(y)
+print(toplam)
+'''
+
+x = 5
+#int to float
+x = float(x)
+print(type(x))
+print(x)
+
+#float to int
+y = 5.5
+y = int(y)
+print(type(y))
+print(y)
+
+#bool to str
+isOnline = True
+isOnline = str(isOnline)
+print(type(isOnline))
+print(isOnline)
+
+#bool to int
+isOnline1 = False
+isOnline1 = int(isOnline1)
+print(isOnline1)
+
+
+     
---
+++ Python_Objeleri/Variables.py
@@ -0,0 +1,25 @@
+# Değişken Tanımlama Kuralları
+
+# Rakam ile başlayamaz
+
+number1 =10
+print(number1)
+
+number1 += 30
+print(number1)
+
+# Büyük küçük harf duyarlılığı vardır.
+# Türkçe karekter kullanmayalım
+# Degisken tanımlanırken boşluk olamaz
+
+age =12
+Age = 45
+print(age)
+print(Age)
+
+x=10           #int
+y=2.1          #float
+name='Berna'   #string
+Istrue = True  #bool
+
+# Çoklu değişken atama    ----- x,y,name,Istrue = (1,2.1,'Berna', True)

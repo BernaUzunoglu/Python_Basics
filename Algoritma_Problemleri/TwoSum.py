d17e565665e0ef35023c1368374b1113895a88aa
bernauzunoglu <bernauzunoglu58@gmail.com>
2024-02-19 16:28:55 +0300

    Two Sum Algorithm problems solved added.

---
+++ Algoritma_Problemleri/TwoSum.py
@@ -0,0 +1,42 @@
+'''
+Given an array of integers nums and an integer target,
+ return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution,
+ and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+'''
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(0,len(nums),1):
+             for j in range(1,len(nums),1):
+                if (nums[i]+nums[j]== target):
+                    return [i,j]
+                break
+
+c=Solution()
+print(c.twoSum([2,7,11,15],9))
+print(c.twoSum([3,2,4],6))
+print(c.twoSum([3,3],6))

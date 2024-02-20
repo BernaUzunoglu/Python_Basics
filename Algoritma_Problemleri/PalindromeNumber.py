89dbd8beda6e4718e7608c62965bd1d23b3853d2
bernauzunoglu <bernauzunoglu58@gmail.com>
2024-02-20 17:13:48 +0300

    Palindrome Number Algorithm problems solved added.

---
+++ Algoritma_Problemleri/PalindromeNumber.py
@@ -0,0 +1,39 @@
+'''
+Given an integer x, return true if x is a 
+palindrome
+, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+'''
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if str(x) == str(x)[::-1] :
+            return True
+        else:
+            return False
+        
+c = Solution() 
+print(c.isPalindrome(123))     
+print(c.isPalindrome(121))     
+print(c.isPalindrome(-121))     
+print(c.isPalindrome(10))     

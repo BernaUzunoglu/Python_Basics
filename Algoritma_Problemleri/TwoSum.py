8e32151b6ed8ba40b816ecdffa9fcef941da0694
bernauzunoglu <bernauzunoglu58@gmail.com>
2024-02-20 17:10:06 +0300

    Two Sum Algorithm problems solved added.

--- Algoritma_Problemleri/TwoSum.py
+++ Algoritma_Problemleri/TwoSum.py
@@ -31,12 +31,17 @@ class Solution(object):
         :rtype: List[int]
         """
         for i in range(0,len(nums),1):
-             for j in range(1,len(nums),1):
+             print("ilk döngü",nums[i])
+             for j in range(i+1,len(nums),1):
+                print(nums[j])
                 if (nums[i]+nums[j]== target):
                     return [i,j]
-                break
+                    break
+            
 
 c=Solution()
 print(c.twoSum([2,7,11,15],9))
 print(c.twoSum([3,2,4],6))
 print(c.twoSum([3,3],6))
+print(c.twoSum([3,2,3],6))
+print(c.twoSum([2,5,5,11],10))
